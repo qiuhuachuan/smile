fccb4f783813d2b3df7ff3a3c95d04db98b4ee8e
qiuhuachuan <qiuhuachuan@yeah.net>
2023-06-12 16:13:40 +0800

    update training script

---
+++ src/research_finetune.sh
@@ -0,0 +1,15 @@
+export WANDB_PROJECT=research_smile
+nohup python -u finetune.py \
+    --dataset_path train_data/smile \
+    --lora_rank 8 \
+    --per_device_train_batch_size 8 \
+    --gradient_accumulation_steps 1 \
+    --num_train_epochs 1 \
+    --save_steps 1000 \
+    --save_total_limit 2 \
+    --learning_rate 1e-4 \
+    --fp16 \
+    --remove_unused_columns false \
+    --logging_steps 10 \
+    --report_to wandb \
+    --output_dir research_smile > ./log/research_smile.log &
